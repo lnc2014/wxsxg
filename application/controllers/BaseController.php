925136aab6713197d23a3c82adc723c584b1280a
lnc <li5nongcheng@126.com>
2016-05-25 23:04:37 +0800

    闪修哥微信端添加

---
+++ application/controllers/BaseController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Description:微信端基础类
+ * Author: LNC
+ * Date: 2016/5/25
+ * Time: 22:51
+ */
+
+class BaseController extends CI_Controller{
+
+    public function __construct(){
+        parent::__construct();
+    }
+}
---
+++ application/controllers/Index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Description:微信端首页
+ * Author: LNC
+ * Date: 2016/5/25
+ * Time: 22:51
+ */
+include_once "BaseController.php";
+
+class Index extends BaseController{
+
+    public function test(){
+        echo "Hello world1";
+    }
+
+    public function index(){
+        echo 1;
+    }
+}
